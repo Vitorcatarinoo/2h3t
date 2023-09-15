a5d1cfdf3ff3ec73ff7c76aab040565f4a804df4
Vitorcatarinoo <127403668+Vitorcatarinoo@users.noreply.github.com>
2023-09-15 08:10:17 -0300

    Add files via upload
    a

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-clicado=() => alert("Alo Mundo")
+clicado=() => alert("jesus lindão");
+mensagem = (msg) => console .log(msg);
+soma = (a,b) => a+b;
+sub = (a,b) => a-b;
+mult = (a,b)=> a * b;
+div = (a,b) => a / b;
+montante_juros_simples = (c,j) => mult(c,div(j,100));
+
+mensagem (soma (15, soma(11, soma(4,7))));
+
+mensagem(sub(18,7));
+
+mensagem(soma(2,div(2,2)));
+
+mensagem(juros_simples(420,18));
